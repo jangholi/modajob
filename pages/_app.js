09428e4eb41b77223cf0bd1955df37e244cbc4db
marjan <marjan.jangholi@yahoo.com>
2021-04-26 23:27:53 +0430

    install and import bootstrap globally in project

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import '../styles/globals.css'
 import DefaultLayout from '../layout/default-layout'
+import 'bootstrap/dist/css/bootstrap.css'
 
 function MyApp({ Component, pageProps }) {
   return (
