ab8aedd91e1967dfaa3d0ba5d5d53429bb4aa990
marjan <marjan.jangholi@yahoo.com>
2021-04-27 17:05:11 +0430

    finish header design

--- layout/default-layout/header.js
+++ layout/default-layout/header.js
@@ -1,9 +1,17 @@
 import React from "react"
+import styles from '../../styles/Header.module.scss'
 
 const Header = () => (
-    <>
-        this is Header
-    </>
+    <div className={styles['header-container']}>
+        <img className={styles.header} src={'/headers/home-header.png'} alt=""/>
+        <div className={"d-flex flex-column " + styles['blur-header']}>
+            <img src="/logo.png" alt=""/>
+            <div className="d-flex full-height">
+                <p className='align-self-center'>Properties in KingSton <br/>
+                    & surrounding areas</p>
+            </div>
+        </div>
+    </div>
 )
 
 export default Header
