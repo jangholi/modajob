6d8eec7577dc130648be617d09cfd98f95ec6d5d
marjan <marjan.jangholi@yahoo.com>
2021-04-29 21:50:33 +0430

    use sub-header in home page instead of default-layout

--- layout/default-layout/index.js
+++ layout/default-layout/index.js
@@ -7,19 +7,6 @@ import Navbar from '../../components/navbar'
 const DefaultLayout = ({children, headerImg}) => (
     <>
         <Header img={headerImg} />
-        <div className={styles['sub-header']}>
-            <div className='row'>
-                <div className={"col-md-6"}>
-                    <p>
-                        left div
-                    </p>
-                </div>
-                <div className={"col-md-6"}>
-                    <Navbar />
-                </div>
-            </div>
-        </div>
-        this is DefaultLayout
         {children}
         <Footer />
     </>
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.scss'
 import DefaultLayout from '../layout/default-layout'
+import Navbar from "../components/navbar";
+import React from "react";
 
 export default function Home() {
   return (
@@ -12,6 +14,21 @@ export default function Home() {
           </Head>
 
           <main className={styles.main}>
+
+              {/*add sub header*/}
+              <div className={styles['sub-header']}>
+                  <div className='row'>
+                      <div className={"col-md-6"}>
+                          <p>
+                              left div
+                          </p>
+                      </div>
+                      <div className={"col-md-6 d-flex justify-content-end"}>
+                          <Navbar />
+                      </div>
+                  </div>
+              </div>
+
               this is main
           </main>
         </div>
