c4fbfeb01b90996dff221a8c3a0d660d6cfdc9bd
marjan <marjan.jangholi@yahoo.com>
2021-05-03 21:37:57 +0430

    improve about us code

--- pages/about.js
+++ pages/about.js
@@ -6,10 +6,10 @@ import React from "react";
 
 export default function About() {
   return (
-      <DefaultLayout headerImg='/images/header/home-header.png' title={['ABOUT', 'CARRINGTONS']} >
+      <DefaultLayout headerImg='/images/about/header.jpg' title={['ABOUT', 'CARRINGTONS']} >
         <div>
           <Head>
-            <title>Home Page</title>
+            <title>About Us</title>
             <link rel="icon" href="/favicon.ico" />
           </Head>
 
@@ -18,10 +18,10 @@ export default function About() {
             {/* sub header */}
             <div className="sub-header">
               <div className='row m0'>
-                <div className={"col-md-1 col-sm-12 " + styles["about-sub-header"]}>
-                  <img src="/images/about/logo-icon.png" />
+                <div className={"col-lg-1 col-md-2 col-sm-3 " + styles["about-section"]}>
+                  <img src="/images/logo-icon.png" />
                 </div>
-                <div className={"col-md-9 col-sm-12 " + styles["about-sub-header"]}>
+                <div className={"col-lg-10 col-md-9 col-sm-9 " + styles["about-section"]}>
                   <p>
                     About usTum lam, as inctur, eiumquae venisquae. Nam non cus ma quunt pe pro conse landit repratia que nonsectate nis enia dolorep.Volorem ossuntibus plam, si optatias es molores toreritat enest ipit, busam dolupturione alit <br/>
                   </p>
@@ -32,7 +32,7 @@ export default function About() {
                     Rectass inullab orepta nulpa velendigenet laboria nonse re serum in pos autempo repellab incipsa nducia qui con nonsequi volest, is dolende volorio reprem cum inum faccabores venitatur rae entiore pres molla alignisincit od que precum eri sus.
                   </p>
                 </div>
-                <div className={"col-md-2 d-flex justify-content-end"}>
+                <div className={"col-lg-1 col-md-1 col-sm-12 order-first order-md-last d-flex justify-content-end mt-3"}>
                   <Navbar />
                 </div>
               </div>
@@ -57,17 +57,17 @@ export default function About() {
             {/* team vidoe */}
             <div className='full-width'>
               <div className='row m0'>
-                <div className={"col-md-4 col-sm-6 " + styles["list-container"]}>
+                <div className={"col-md-4 col-sm-6 " + styles["video-container"]}>
                   <img src="/images/about/team1.jpg" alt=""/>
-                  <div className={"d-flex full-height " + styles["blur-header"]}></div>
+                  <div className={"d-flex full-height " + styles["blur-div"]}></div>
                 </div>
-                <div className={"col-md-4 col-sm-6 " + styles["list-container"]}>
+                <div className={"col-md-4 col-sm-6 " + styles["video-container"]}>
                   <img src="/images/about/team2.jpg" alt=""/>
-                  <div className={"d-flex full-height " + styles["blur-header"]}></div>
+                  <div className={"d-flex full-height " + styles["blur-div"]}></div>
                 </div>
-                <div className={"col-md-4 col-sm-6 " + styles["list-container"]}>
+                <div className={"col-md-4 col-sm-6 " + styles["video-container"]}>
                   <img src="/images/about/team3.jpg" alt=""/>
-                  <div className={"d-flex full-height " + styles["blur-header"]}></div>
+                  <div className={"d-flex full-height " + styles["blur-div"]}></div>
                 </div>
               </div>
             </div>
@@ -96,13 +96,13 @@ export default function About() {
               </div>
             </div>
 
-            {/* about description */}
+            {/* about */}
             <div className="sub-header">
               <div className='row m0'>
-                <div className={"col-md-1 col-sm-12 " + styles["about-sub-header"]}>
-                  <img src="/images/about/quots.png" />
+                <div className={"col-lg-1 col-md-2 col-sm-3 " + styles["about-section"]}>
+                  <img src="/images/quotation.png" />
                 </div>
-                <div className={"col-md-9 col-sm-12 " + styles["about-sub-header"]}>
+                <div className={"col-lg-11 col-md-10 col-sm-9 " + styles["about-section"]}>
                   <p>
                     Carringtons recently dealt with the probate sale of a flat for me. Finding a buyer was made more difficult due to the difficulty in getting a mortgage for a flat in this particular building. However Callum was proactive in finding a buyer and was very diligent in following up the sale; dealing well with a few problems. All the other staff that I had dealings with were also very helpful.
                   </p>
@@ -110,7 +110,7 @@ export default function About() {
               </div>
             </div>
 
-            {/* about process */}
+            {/* process */}
             <div>
               <div className='row m0 p40'>
                 <div className={"col-md-12 col-sm-12 " + styles["process-section"]}>
