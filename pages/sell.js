68a5e5bb352e6c358745983333950cae4585e298
marjan <marjan.jangholi@yahoo.com>
2021-05-03 22:40:38 +0430

    improve sell page

--- pages/sell.js
+++ pages/sell.js
@@ -6,10 +6,10 @@ import React from "react";
 
 export default function Sell() {
     return (
-        <DefaultLayout headerImg='/images/header/home-header.png' title={['SELL your property', 'WITH CARRINGTONS']} >
+        <DefaultLayout headerImg='/images/about/header.jpg' title={['SELL your property', 'WITH CARRINGTONS']} >
             <div>
                 <Head>
-                    <title>Home Page</title>
+                    <title>Sell</title>
                     <link rel="icon" href="/favicon.ico" />
                 </Head>
 
@@ -18,15 +18,15 @@ export default function Sell() {
                     {/* sub header */}
                     <div className="sub-header">
                         <div className='row m0'>
-                            <div className={"col-md-1 col-sm-12 " + styles["about-sub-header"]}>
-                                <img src="/images/quots.png" />
+                            <div className={"col-lg-1 col-md-2 col-sm-3 " + styles["about-sub-header"]}>
+                                <img src="/images/quotation.png" />
                             </div>
-                            <div className={"col-md-9 col-sm-12 " + styles["about-sub-header"]}>
+                            <div className={"col-lg-10 col-md-9 col-sm-9 " + styles["about-sub-header"]}>
                                 <p>
                                     From the outset, it was clear that our property would be a unique sale due to the fact that it was both commercial and residential. Despite this, Carringtons were open and honest with us and helped us to find a suitable buyer. Every step along the way, Alex and his team have spent hours of time in chasing paperwork, ringing buyers and giving us sound advice so that we would get the best possible outcome. As a family, we feel that Carringtons really couldn't have done anymore for us and we are exceptionally grateful. We feel that the team, especially Alex, went above and beyond what was expected of them. Thank you once again for your professionalism, swiftness and commitment.
                                 </p>
                             </div>
-                            <div className={"col-md-2 d-flex justify-content-end"}>
+                            <div className={"col-lg-1 col-md-1 col-sm-12 order-first order-md-last d-flex justify-content-end mt-3"}>
                                 <Navbar />
                             </div>
                         </div>
@@ -35,10 +35,10 @@ export default function Sell() {
                     {/*video section*/}
                     <div className='full-width'>
                         <div className={"d-flex row m0 " + styles["about"]}>
-                            <div className={"col-md-1 col-sm-2 " + styles["about-sub-header"]}>
+                            <div className={"col-lg-1 col-md-2 col-sm-3 " + styles["about-sub-header"]}>
                                 <img src="/images/logo-icon.png" />
                             </div>
-                            <div className={"col-md-5 col-sm-10 d-flex align-self-center " + styles['about-left-section']}>
+                            <div className={"col-lg-5 col-md-5 col-sm-9 d-flex align-self-center " + styles['about-left-section']}>
                                 <div>
                                     <p>
                                         Inctur, eiumquae venisquae. Nam non cus ma quunt pe pro conse landit repratia que nonsectate nis enia dolorep.Volorem ossuntibus plam, si optatias es molores toreritat enest ipit, busam dolupturione alit
@@ -51,7 +51,7 @@ export default function Sell() {
                                     </p>
                                 </div>
                             </div>
-                            <div className={"col-md-6 col-sm-0 " + styles["about-right-section"]}>
+                            <div className={"col-lg-6 col-md-5 col-sm-12 " + styles["about-right-section"]}>
                                 <img src="/images/home/about.jpg" className='pt30' />
                             </div>
                         </div>
