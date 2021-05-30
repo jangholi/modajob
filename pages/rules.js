7305b61f78e7851fe26e641a19c8372a8937f892
marjan <marjan.jangholi@yahoo.com>
2021-05-30 11:14:40 +0430

    create rules page structure

---
+++ pages/rules.js
@@ -0,0 +1,25 @@
+import React from "react";
+import Head from 'next/head';
+import EmployeeLayout from '../layout/employee-layout';
+
+export default function Rules() {
+
+    return (
+        <EmployeeLayout>
+            <div>
+
+                <Head>
+                    <title>جستجوی مشاغل</title>
+                    <link rel="icon" href="/favicon.ico" />
+                </Head>
+
+                <main>
+
+                    <p>rules</p>
+
+                </main>
+
+            </div>
+        </EmployeeLayout>
+    )
+}
