c722200f2f3a11bed662322efefa5019025a15e0
marjan <marjan.jangholi@yahoo.com>
2021-05-30 11:14:17 +0430

    create contact us page structure

---
+++ pages/contact.js
@@ -0,0 +1,26 @@
+import React from "react";
+import Head from 'next/head';
+import EmployeeLayout from '../layout/employee-layout';
+
+
+export default function Contact() {
+
+    return (
+        <EmployeeLayout>
+            <div>
+
+                <Head>
+                    <title>جستجوی مشاغل</title>
+                    <link rel="icon" href="/favicon.ico" />
+                </Head>
+
+                <main>
+
+                    <p>contact</p>
+
+                </main>
+
+            </div>
+        </EmployeeLayout>
+    )
+}
